d878fd74397c93fc46bb252239828d32e154a336
Powarox219 <moriniere.robin1877@gmail.com>
2021-10-31 19:52:01 +0100

    Css navigation bar

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,21 @@
 <template>
     <div id="app">
         <div id="nav">
+            <div>Logo</div>
             <router-link to="/">Home</router-link>
+            <router-link to="/">Page 1</router-link>
+            <router-link to="/">Page 2</router-link>
+            <router-link to="/">Page 3</router-link>
+            <router-link to="/">About</router-link>
             <!-- <router-link to="/Login">Login</router-link> -->
         </div>
 
         <router-view/>
+
+        <div id="footer">
+            <p>Moriniere Robin 21606393</p>
+            <p>Mohamed Lamine Seck 21711412</p>
+        </div>
     </div>
 </template>
 
@@ -19,12 +29,62 @@
 </script>
 
 <style>
+    body {
+        margin: 0;
+    }
+
     #app {
       font-family: Avenir, Helvetica, Arial, sans-serif;
       -webkit-font-smoothing: antialiased;
       -moz-osx-font-smoothing: grayscale;
-      text-align: center;
+      /* text-align: center; */
       color: #2c3e50;
-      margin-top: 60px;
+      margin: 0;
     }
+
+/* --- Navigation Bar --- */
+    #nav {
+        display: flex;
+        justify-content: left;
+        align-items: center;
+        margin: 0;
+        padding: 10px;
+        background: #E56B6F;
+        font-weight: bold;
+
+    }
+
+    #nav a {
+        margin: 10px 20px;
+        text-decoration: none;
+        color: #333;
+        border-bottom: 3px solid #E56B6F;
+        transition: 0.5s;
+    }
+
+    #nav a:hover {
+        border-bottom: 3px solid white;
+    }
+
+    #nav div {
+        margin: 0 10px;
+        color: #DDD;
+        background: #355070;
+        padding: 17px;
+        border-radius: 5px;
+        cursor: pointer;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
 </style>
