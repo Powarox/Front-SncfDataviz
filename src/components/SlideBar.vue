d09d8c4f497cdbd25ee11a38ed23280ea33aa774
Powarox219 <moriniere.robin1877@gmail.com>
2021-12-20 16:56:12 +0100

    Test implement API

--- src/components/SlideBar.vue
+++ src/components/SlideBar.vue
@@ -8,17 +8,50 @@
             <button type="button" name="button">test 3</button>
             <button type="button" name="button">test 4</button>
         </section>
+
+        <section>
+            <div v-for="currency in info" class="currency">
+                {{ currency.description }}:
+
+                <span class="lighten">
+                    <span v-html="currency.symbol"></span>
+                    {{ currency.rate_float | currencydecimal }}
+                </span>
+            </div>
+        </section>
     </div>
 </template>
 
 <script>
+    import axios from "axios";
+
     export default {
         name: "Slidebar",
         data() {
             return {
 
             }
-        }
+        },
+
+        filters: {
+            currencydecimal (value) {
+                return value.toFixed(2)
+            }
+        },
+
+        mounted () {
+            axios
+                .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+                .then(response => {
+                    this.info = response.data.bpi
+                })
+                .catch(error => {
+                    console.log(error)
+                    this.errored = true
+                })
+                .finally(() => this.loading = false)
+        },
+
     }
 </script>
 
