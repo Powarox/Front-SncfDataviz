a9b297d982abf287f9ed6a0a68c930b67ccf3266
Powarox219 <moriniere.robin1877@gmail.com>
2021-12-21 16:02:41 +0100

    Add title page and 404 error

---
+++ src/views/NotFound.vue
@@ -0,0 +1,25 @@
+<template lang="html">
+    <div id="notFound">
+        <h1>404 Page Not Found</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style lang="css" scoped>
+    #notFound {
+        height: 500px;
+        padding: 30px;
+        background: #FAE2E3;
+    }
+
+    #notFound {
+        display: grid;
+        justify-content: space-around;
+        align-content: space-around;
+    }
+</style>
