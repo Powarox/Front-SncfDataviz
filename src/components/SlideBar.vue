70a03c84086f13df1a34977ae643d96b2db42e7d
Powarox219 <moriniere.robin1877@gmail.com>
2021-12-21 17:30:48 +0100

    Moove test API

--- src/components/SlideBar.vue
+++ src/components/SlideBar.vue
@@ -8,60 +8,12 @@
             <button type="button" name="button">test 3</button>
             <button type="button" name="button">test 4</button>
         </section>
-
-        <!-- <section>
-            <div v-for="currency in info" class="currency">
-                {{ currency.description }}:
-
-                <span class="lighten">
-                    <span v-html="currency.symbol"></span>
-                    {{ currency.rate_float | currencydecimal }}
-                </span>
-            </div>
-        </section> -->
-
-        <section v-if="errored">
-            <p>Nous sommes désolés, nous ne sommes pas en mesure de récupérer ces informations pour le moment. Veuillez réessayer ultérieurement.</p>
-        </section>
-
-        <section v-else>
-            <div v-if="loading">Chargement...</div>
-
-            <div v-else v-for="currency in info" :key="currency" class="currency">
-                {{ currency.description }}:
-                {{ currency.rate_float }}
-            </div>
-        </section>
     </div>
 </template>
 
 <script>
-    import axios from "axios";
-
     export default {
         name: "Slidebar",
-        data() {
-            return {
-                info: null,
-                loading: true,
-                errored: false
-            }
-        },
-
-        mounted () {
-            axios
-                .get('https://api.coindesk.com/v1/bpi/currentprice.json')
-                .then(response => {
-                    this.info = response.data.bpi
-                    console.log(this.info);
-                })
-                .catch(error => {
-                    console.log(error)
-                    this.errored = true
-                })
-                .finally(() => this.loading = false)
-        },
-
     }
 </script>
 
--- src/views/Page2.vue
+++ src/views/Page2.vue
@@ -3,12 +3,48 @@
         <a href="https://www.datavis.fr/index.php?page=map-hexgrid" target="_blank">Carte hexa ville</a><br>
         <a href="https://www.datavis.fr/index.php?page=map-firststep" target="_blank">Carte régions</a><br>
         <a href="https://www.datavis.fr/index.php?page=map-population" target="_blank">Carte régions couleur</a>
+
+        <section v-if="errored">
+            <p>Nous sommes désolés, nous ne sommes pas en mesure de récupérer ces informations pour le moment. Veuillez réessayer ultérieurement.</p>
+        </section>
+
+        <section v-else>
+            <div v-if="loading">Chargement...</div>
+
+            <div v-else v-for="currency in info" :key="currency" class="currency">
+                {{ currency.description }}:
+                {{ currency.rate_float }}
+            </div>
+        </section>
     </div>
 </template>
 
 <script>
+    import axios from "axios";
+
     export default {
         name: 'Page2',
+        data() {
+            return {
+                info: null,
+                loading: true,
+                errored: false
+            }
+        },
+
+        mounted () {
+            axios
+                .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+                .then(response => {
+                    this.info = response.data.bpi
+                    console.log(this.info);
+                })
+                .catch(error => {
+                    console.log(error)
+                    this.errored = true
+                })
+                .finally(() => this.loading = false)
+        },
     }
 </script>
 
