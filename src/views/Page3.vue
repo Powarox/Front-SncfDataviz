6d466d9a2b1aceddaa7ca071612f9093344901ac
Powarox2159 <moriniere.robin1877@gmail.com>
2022-01-04 19:32:17 +0100

    Create Chart Components

--- src/views/Page3.vue
+++ src/views/Page3.vue
@@ -7,67 +7,26 @@
 
         <br><br>
 
-        <section v-if="errored">
-            <p>Nous sommes désolés, nous ne sommes pas en mesure de récupérer ces informations pour le moment. Veuillez réessayer ultérieurement.</p>
-        </section>
-
-        <section v-else>
-            <div v-if="loading">Chargement...</div>
-
-            <div v-else v-for="currency in info" :key="currency" class="currency">
-                {{ currency.description }}:
-                {{ currency.rate_float }}
-            </div>
-        </section>
-
-        <br><br>
-
         <section class='test'>
-
+            <BarChart/>
         </section>
     </div>
 </template>
 
 <script>
-    import axios from "axios";
+    import BarChart from '../components/BarChart.vue';
 
     export default {
         name: 'Page2',
+        components: {
+            BarChart
+        },
         data() {
             return {
-                info: null,
-                loading: true,
-                errored: false
+
             }
         },
 
-        mounted () {
-            // Get bitcoin price
-            axios
-                .get('https://api.coindesk.com/v1/bpi/currentprice.json')
-                .then(response => {
-                    this.info = response.data.bpi
-                    // console.log(this.info);
-                })
-                .catch(error => {
-                    console.log(error)
-                    this.errored = true
-                })
-                .finally(() => this.loading = false),
-
-            // Get worksite list
-            axios
-                .get('https://data.sncf.com/api/records/1.0/search/?dataset=liste-des-chantiers&q=&rows=5')
-                .then(response => {
-                    console.log(response.data.records);
-
-                })
-                .catch(error => {
-                    console.log(error)
-                    this.errored = true
-                })
-                .finally(() => this.loading = false)
-        },
     }
 </script>
 
