0b39e3398d418d9df8a4e23b037a04c662357c15
momooz10 <momo-neymar10@hotmail.de>
2022-01-05 21:36:53 +0100

    Create Components Api

--- src/api/Api.vue
+++ src/api/Api.vue
@@ -1,137 +1,58 @@
-<template>
-    <div id="Page2">
-        <section class='test'>
-            <div v-if="loading1">Chargement ... Mouvement sociaux</div>
-            <div v-if="loading2">Chargement ... Liste des gares</div>
-            <div v-if="loading3">Chargement ... Liste des chatiers</div>
-            <div v-if="loading4">Chargement ... Regularité Mensuelle</div>
-            <button @click="test()">test</button>
-            <p>{{ this.data1 }}</p><br>
-            <!-- <p>{{ this.data2 }}</p><br> -->
-            <!-- <p>{{ this.data3 }}</p><br> -->
-            <!-- <p>{{ this.data4 }}</p><br> -->
-        </section>
-    </div>
-</template>
-
 <script>
     import axios from "axios";
+    import { mapActions } from 'vuex';
 
     export default {
-        name: 'Page2',
+        name: 'Api',
         data() {
             return {
                 info: null,
                 errored: false,
-                loading1: true,
-                loading2: true,
-                loading3: true,
-                loading4: true,
-
-                data1: {},
-                data2: [],
-                data3: [],
-                data4: {},
-                data5: {},
+                loading: true,
+                dataset1: {},
+                dataset2: {},
+                dataTmp: {},
             }
         },
 
         methods: {
-            test(){
-                for(let date in this.data4){
-                    for(let key in this.data1[date]){
-                        this.data4[date][key] = this.data1[date][key]
-                    }
-                    if(this.data4[date]['journees_perdues']){
-                        console.log('ok');
-                    }
-                    else {
-                        console.log('not here');
+            ...mapActions([
+                'updateData',
+            ]),
+
+            GlobalTrainLate(){
+                for(let res in this.dataset1){
+                    this.dataset2['global_train'] += this.dataset1[res]['nb_train_prevu'];
+                    this.dataset2['nb_tot_ann'] += this.dataset1[res]['nb_annulation'];
+                    this.dataset2['nb_ret_dep'] += this.dataset1[res]['nb_train_retard_depart'];
+                    this.dataset2['nb_ret_arr'] += this.dataset1[res]['nb_train_retard_arrivee'];
+                    this.dataset2['nb_ret_s15'] += this.dataset1[res]['nb_train_retard_sup_15'];
+                    this.dataset2['nb_ret_s30'] += this.dataset1[res]['nb_train_retard_sup_30'];
+                    this.dataset2['nb_ret_s60'] += this.dataset1[res]['nb_train_retard_sup_60'];
+                }
+                this.dataset2['nb_in_time'] = this.dataset2['global_train'] - this.dataset2['nb_ret_arr'] - this.dataset2['nb_tot_ann']
+            },
+
+            addDataFromOtherAPI(){
+                for(let date in this.dataset1){
+                    for(let key in this.dataTmp[date]){
+                        this.dataset1[date][key] = this.dataTmp[date][key]
                     }
                 }
-                console.log(this.data4);
-            }
+            },
+
+            upgradDatabase(){
+                this.updateDataSet1(this.dataset1);
+                this.updateDataSet2(this.dataset2);
+            },
+
         },
 
-        mounted () {
-            // Get Mouvement sociaux
-            axios
-                .get('https://data.sncf.com/api/records/1.0/search/?dataset=mouvements-sociaux-depuis-1994&q=&rows=-1&sort=date')
-                .then(response => {
-                    let results = response.data.records;
-                    let stop = false;
-
-                    for(let res in results){
-                        if(!stop){
-                            if(results[res].fields['date'] === '2018-02'){
-                                this.data1[results[res].fields['date']] = {
-                                    'journees_perdues': results[res].fields['journees_perdues']
-                                }
-                                stop = true;
-                            }
-                            else {
-                                this.data1[results[res].fields['date']] = {
-                                    'journees_perdues': results[res].fields['journees_perdues']
-                                }
-                            }
-                        }
-                    }
-                })
-                .catch(error => {
-                    console.log(error)
-                    this.errored = true
-                })
-                .finally(() => this.loading1 = false)
-
-            // Get worksite list
-            axios
-                .get('https://data.sncf.com/api/records/1.0/search/?dataset=liste-des-chantiers&q=&rows=10')
-                .then(response => {
-                    let results = response.data.records;
-
-                    for(let res in results){
-                        this.data2 .push({
-                            'gare': results[res].fields.gare,
-                            'libelle': results[res].fields.libelle,
-                            'lattitude': results[res].fields.geo_point_2d[0],
-                            'longitude': results[res].fields.geo_point_2d[1],
-                        })
-                    }
-                })
-                .catch(error => {
-                    console.log(error)
-                    this.errored = true
-                })
-                .finally(() => this.loading2 = false)
-
-            // Get subway station list
-            axios
-                .get('https://data.sncf.com/api/records/1.0/search/?dataset=liste-des-gares&q=&rows=10')
-                .then(response => {
-                    let results = response.data.records;
-
-                    for(let res in results){
-                        this.data3.push({
-                            'gare': results[res].fields.libelle,
-                            'commune': results[res].fields.commune,
-                            'departement': results[res].fields.departemen,
-                            'lattitude': results[res].fields.geo_point_2d[0],
-                            'longitude': results[res].fields.geo_point_2d[1],
-                        })
-                    }
-                })
-                .catch(error => {
-                    console.log(error)
-                    this.errored = true
-                })
-                .finally(() => this.loading3 = false)
-
-            // // Get Retard train
+        mounted() {
             axios
                 .get('https://data.sncf.com/api/records/1.0/search/?dataset=regularite-mensuelle-tgv-aqst&q=&rows=-1&sort=date')
                 .then(response => {
                     let results = response.data.records;
-                    let newTab = {};
                     let tmp = '';
 
                     let duree_moyenne = 0;
@@ -171,7 +92,7 @@
                         }
                         else {
                             if(tmp !== ''){
-                                newTab[tmp] = {
+                                this.dataset1[tmp] = {
                                     'duree_moyenne' : duree_moyenne,
                                     'nb_annulation' : nb_annulation,
                                     'nb_train_prevu' : nb_train_prevu,
@@ -205,23 +126,43 @@
                             tmp = date;
                         }
                     }
-                    this.data4 = newTab;
+                    this.GlobalTrainLate();
+                    this.addDataFromOtherAPI();
+                    this.upgradDatabase();
                 })
                 .catch(error => {
                     console.log(error)
                     this.errored = true
                 })
-                .finally(() => this.loading4 = false)
-        },
+                .finally(() => this.loading = false)
+
+            axios
+                .get('https://data.sncf.com/api/records/1.0/search/?dataset=mouvements-sociaux-depuis-1994&q=&rows=-1&sort=date')
+                .then(response => {
+                    let results = response.data.records;
+                    let stop = false;
+
+                    for(let res in results){
+                        if(!stop){
+                            if(results[res].fields['date'] === '2018-02'){
+                                this.dataTmp[results[res].fields['date']] = {
+                                    'journees_perdues': results[res].fields['journees_perdues']
+                                }
+                                stop = true;
+                            }
+                            else {
+                                this.dataTmp[results[res].fields['date']] = {
+                                    'journees_perdues': results[res].fields['journees_perdues']
+                                }
+                            }
+                        }
+                    }
+                })
+                .catch(error => {
+                    console.log(error)
+                    this.errored = true
+                })
+                .finally(() => this.loading = false)
+        }
     }
 </script>
-
-<style lang="css" scoped>
-    #Page2 {
-
-    }
-
-    p {
-        margin: 0;
-    }
-</style>
